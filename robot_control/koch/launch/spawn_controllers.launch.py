f7b68e21d83db7928803a6f258b2d4d2e9093a6d
Eldor Fozilov <eldorfozilov@gmail.com>
2024-11-27 17:02:29 +0900

    basic solution

--- robot_control/koch/launch/spawn_controllers.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("low_cost_robot", package_name="koch").to_moveit_configs()
-    return generate_spawn_controllers_launch(moveit_config)
